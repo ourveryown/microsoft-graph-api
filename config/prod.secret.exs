303bdcc98303572141eb5f2d13292eef40eece50
Errol-Hassall-OVO <ehassall@ourveryown.com.au>
2018-11-02 09:50:45 +1100

    took all files out of the folder and put the project in the root git directory

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :microsoft_graph_api, MicrosoftGraphApiWeb.Endpoint,
+  secret_key_base: "YJR3/PlK+6t4ZZYbnfc5LF5MQvJ94P7KELODcE8vr3RHj1kKN4xi22R/XrRbfy7I"
